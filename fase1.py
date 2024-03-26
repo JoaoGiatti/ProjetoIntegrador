371a72a587743b0a43c91f044df4108a9541a29c
JoaoGiatti <joaogiatti2006@gmail.com>
2024-03-26 10:46:12 -0300

    inserção de dados

--- fase1.py
+++ fase1.py
@@ -1,2 +1,10 @@
 #inserção de dados:
-id = int(input("digite o id: "))
+id = int(input("digite o id do produto: "))
+nome = input("digite o nome do produto: ")
+desc = input("digite a descrição do produto: ")
+ca = float(input("digite o custo do produto: "))
+cf = float(input("digite o custo fixo/administrativo: "))
+cv = float(input("digite o valor da comissão de vendas: "))
+iv = float(input("digite o valor do imposto sobre o produto: "))
+ml = float(input("digite o valor da rentabilidade "))
+
